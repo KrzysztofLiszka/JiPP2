cf70e712a8613668247c52fde7d60275b2593575
Krzysztof Liszka <krzysztof.liszkaa@wp.pl>
2021-10-13 16:11:54 +0200

    lab1

---
+++ LAB1/allocation_basics/cmake-build-debug/CMakeFiles/Zajecia1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Zajecia1.dir/C_/Users/Krzysztof/Documents/GitHub/JiPP2/simpleCalc/src/calc.cpp.obj"
+  "CMakeFiles/Zajecia1.dir/C_/Users/Krzysztof/Documents/GitHub/JiPP2/simpleCalc/src/main.cpp.obj"
+  "Zajecia1.exe"
+  "Zajecia1.exe.manifest"
+  "Zajecia1.pdb"
+  "libZajecia1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Zajecia1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
